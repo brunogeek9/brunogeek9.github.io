0a7cef0db4c7264f7c044c7ae88630b3dc7987c0
brunogeek9 <brunogeek9@gmail.com>
2020-05-10 11:59:19 -0300

    alterações na sessão de projetos.

--- js/projects.js
+++ js/projects.js
@@ -5,36 +5,45 @@ let projects = [
         name: "be the hero",
         url: "https://github.com/brunogeek9/be-the-hero",
         languages: "expo, react-native, node-js and axios.",
-        print: "/img/profile_.jpeg",
-        description: "project created within omiweak 11 with the aim of training knowledge while connecting people and ONGs in my region"
-    },
-    {
-        name: "hackernews clone",
-        url: "https://github.com/brunogeek9/hackernews-clone",
-        languages: "vuejs and axios.",
-        print: "/img/profile_.jpeg",
-        description: "hackernews clone created with axios and vuejs."
+        print: {
+            file: "/img/tela1.png",
+            h: "400px",
+            w: "700px"
+        },
+        description: "project created within omiweak 11 with the aim of training knowledge while connecting people and ONGs in my region."
     },
     {
         name: "task manager",
         languages: "PHP with cakephp",
         url: "https://github.com/brunogeek9/task_manager",
-        print: "/img/profile_.jpeg",
-        description: "Task manager web app implemented with cakephp + mysql + bootstrap 4 "
+        print: {
+            file: "",
+            h: "600px",
+            w: "200px"
+        },
+        description: "Task manager web app implemented with cakephp + mysql + bootstrap 4. "
     },
     {
         name: "weather bot",
         url: "https://github.com/brunogeek9/weather-bot",
         languages: "JS",
-        print: "/img/profile_.jpeg",
+        print: {
+            file: "/img/tela3.jpg",
+            h: "500px",
+            w: "300px"
+        },
         description: "bot for the telegram implemented in nodejs with the node-telegram-bot-api library and the openweathermap API."
     },
     {
         name: "Gitfoot",
-        url: "https://github.com/brunogeek9/weather-bot",
+        url: "https://github.com/brunojamelli/GitfootOlheiro",
         languages: "Firebase, google maps api and android native api.",
-        print: "/img/profile_.jpeg",
-        description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum recusandae illum laudantium, sint libero numquam molestias ipsum nesciunt beatae asperiores quos laborum impedit omnis est voluptates, eligendi, veritatis obcaecati! Molestias."
+        print: {
+            file: "/img/tela4.jpeg",
+            h: "500px",
+            w: "300px"
+        },
+        description: "desc"
     },
 ]
 console.log(projects);
@@ -46,17 +55,17 @@ const addInDoom = async() => {
                 <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
                     <div class="resume-content">
                         <h3 class="mb-0">${name}</h3>
-                        <div class="subheading mb-3">${description}</div>
-                        <p>Used in project: ${languages}</p>
+                        <div class="subheading mb-3"> 
+                            <span class="text-primary">
+                                ${description}
+                            </span>
+                        </div>
                         <a href="${url}" target="_black">
-                        <img src="${print}" alt="..." class="img-thumbnail" width="400px"
-                        heidth="400px">
+                        <img src="${print.file}" alt="..." style="width: ${print.w}; height: ${print.h}">
                         </a>
                     </div>
-                    <div class="resume-date text-md-right">
-                        
-                    </div>
                 </div>
+                <hr>
             </div>
         
         `).join('')
